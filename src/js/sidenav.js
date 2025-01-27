2348bff66e07572176eb0088e34e6c60eb69dbbd
Aled <arnold.dc.it@gmail.com>
2025-01-27 11:43:52 +0800

    update

--- src/js/sidenav.js
+++ src/js/sidenav.js
@@ -1,14 +1,8 @@
 // Function to open or close the side nav and move the content
-function toggleNav() {
-    var sidenav = document.getElementById("sidenav");
-    var mainContent = document.getElementById("main-content");
-  
-    if (sidenav.style.left === "0px") {
-      sidenav.style.left = "-100%"; // Close the side nav
-      mainContent.style.marginLeft = "0"; // Move content back
-    } else {
-      sidenav.style.left = "0"; // Open the side nav
-      mainContent.style.marginLeft = "180px"; // Move content to the right
-    }
-  }
+function toggleSidebar() {
+  var sidebar = document.getElementById('sidebar');
+  var content = document.getElementById('content');
+  sidebar.classList.toggle('minimized');
+  content.classList.toggle('minimized');
+}
   
